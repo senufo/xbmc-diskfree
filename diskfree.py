5b1d01efdcb78f33038ed04ca3b836d679247480
senufo <fce.valeins@libertysurf.fr>
2012-01-19 20:21:50 +0100

    First functionnal version

--- diskfree.py
+++ diskfree.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from xbmcgui import Window, WindowDialog
 import xbmc, xbmcgui
 import xbmcaddon
 #python modules
@@ -18,6 +19,14 @@ __language__   = Addon.getLocalizedString
 __profile__    = xbmc.translatePath(Addon.getAddonInfo('profile'))
 __resource__   = xbmc.translatePath(os.path.join(__cwd__, 'resources',
                                                 'lib'))
+DEBUG_LOG = True
+#Function Debug
+def debug(msg):
+    """
+    print message if DEBUG_LOG == True
+    """
+    if DEBUG_LOG == True: print " [%s] : %s " % (__scriptid__, msg)
+
 
                                                  
 #Position du texte
@@ -78,92 +87,23 @@ ImgBoxDiskId = None
 start_time = 0 
 re_added_control = False
 
-#Verifie que xbmc tourne
-while (not xbmc.abortRequested):
-    #Attente avant de  tester l'espace sur le disque
-    #intervalle = int(float(Addon.getSetting('time')) * 60.0)
-    intervalle = 60
-    if start_time and (time.time() - start_time) < intervalle:
-        time.sleep(.5)
-        #SHOW_UPDATE     = Addon.getSetting('show_update') == "true"
-        SHOW_UPDATE     = "true"
-        if ImgBoxDisk:
-            # optional show le temps qu'il reste avant la prochaine MAJ
-            try:
-                if SHOW_UPDATE:
-                    up = int(intervalle) - (time.time() - start_time)
-                    #locstr = Addon.getLocalizedString(615)  #Update in %i second
-                    locstr = "Update in %i second"
-                    #print "MSG up = %s " % msg
-                    label = "%s[CR]" %  msg + locstr % up
-                else: #Il faut rafraichir l'affichage
-                    #print "MSG = %s " % msg
-                    label = '%s' % msg
-                #MsgBoxDisk.setLabel( msg )
-                ImgBoxDisk.setImage(filename)
-                #else: MsgBoxDisk.setLabel( '' )
-            except Exception, e:
-                print str(e)
+#Recupère les arguments envoyés par le skin qui a lancé le script
+for arg in sys.argv:
 
-        HomeNotVisible = xbmc.getCondVisibility( "!Window.IsVisible(10000)" )
-        if HomeNotVisible:
-            #oop! on est plus sur le home
-            re_added_control = True
-        elif re_added_control and not HomeNotVisible:
-            #MsgBoxDisk = xbmcgui.ControlLabel( x, y, width, height, '', font, color )
-            ImgBoxDisk = xbmcgui.ControlImage(x, y, width, height, filename)
-            # add control label and set default label
-            #homeWin.addControl( MsgBoxDisk )
-            homeWin.addControl( ImgBoxDisk )
-            # get control id
-            #MsgBoxDiskId = MsgBoxDisk.getId()
-            ImgBoxDiskId = ImgBoxDisk.getId()
-            re_added_control = False
-            # reload addon setting possible change
-            Addon = xbmcaddon.Addon( __scriptid__ )
+    param = str(arg).lower()
+    debug("param = %s " % param)
+    if 'disk=' in param:
+        disk = param.replace('disk=', '')
+#On récupère l'ID de la fenêtre de skin qui à lancer le script
+okno = Window(xbmcgui.getCurrentWindowId())
 
-        # continue le while sans faire le reste
-        continue
-
-    homeWin = xbmcgui.Window(WINDOW_HOME)
-
-    #xbmc.executebuiltin( "SetProperty(username,'eeee',10000)" )
-    if ImgBoxDiskId:
-        try: 
-            #MsgBoxDisk = homeWin.getControl( MsgBoxDiskId )
-            ImgBoxDisk = homeWin.getControl( ImgBoxDiskId )
-        except: 
-            #MsgBoxId = None
-            ImgBoxId = None
-    if ImgBoxDiskId is None:
-        #MsgBoxDisk = xbmcgui.ControlLabel( x, y, width, height, '', font, color )
-        ImgBoxDisk = xbmcgui.ControlImage( x, y+30, width, height, filename)
-        #homeWin.addControl( MsgBoxDisk )
-        #homeWin.addControl( ImgBoxDisk )
-        # get control id
-        #MsgBoxDiskId = MsgBoxDisk.getId()
-        ImgBoxDiskId = ImgBoxDisk.getId()
-    locstr = Addon.getLocalizedString(616) #Mise a jour
-    #MsgBoxDisk.setLabel( locstr % ' ' )
-    #ImgBoxDisk.setImage(filename)
-
-    #On vide le message
-    msg = ''
-
-    #On recupere les parametres des disques
-    #time = time.time()
-    freespace = freespace_disk('/home/')
-    totalspace = totalspace_disk('/home')
-    diskfree = ('%iGB') % (((freespace / 1024) / 1024) / 1024)
-    freespace = (((freespace / 1024) / 1024) / 1024)
-    totalspace = (((totalspace / 1024) / 1024) / 1024)
-    percent = totalspace and (totalspace - freespace) * 1.0 / totalspace or 0.0
-    print "f = %s, t = %s Pou = %s " % (freespace,totalspace,percent)
-    msg = "f = %s, t = %s Pou = %s " % (freespace,totalspace,percent) 
-
-    
-    #initialise start time
-    start_time = time.time()
-    time.sleep( .5 )
-
-
+#On recupere les parametres des disques
+#time = time.time()
+freespace = freespace_disk('/home/')
+totalspace = totalspace_disk('/home')
+diskfree = ('%iGB') % (((freespace / 1024) / 1024) / 1024)
+freespace = (((freespace / 1024) / 1024) / 1024)
+totalspace = (((totalspace / 1024) / 1024) / 1024)
+percent = totalspace and (totalspace - freespace) * 1.0 / totalspace or 0.0
+debug( "f = %s, t = %s Pou = %s " % (freespace,totalspace,percent))
+okno.setProperty('DISK' , '%s Go' % freespace )
